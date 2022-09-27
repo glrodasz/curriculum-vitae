204145599bd4117851add5c4e16e234c29313189
Guillermo Rodas <me@guillermorodas.com>
2022-09-27 23:10:44 +0200

    Footer in desktop

--- components/atoms/Link.js
+++ components/atoms/Link.js
@@ -3,11 +3,25 @@ import React from "react";
 import Heading from "./Heading";
 import Paragraph from "./Paragraph";
 
-const Link = ({ href, children, weight, isCentered }) => {
+const Link = ({
+  href,
+  color = "secondary",
+  children,
+  weight,
+  isCentered,
+  isInverted,
+}) => {
   return (
     <>
       <a href={href} target="_blank">
-        <Heading size="xxs" color="secondary" weight={weight} isCentered={isCentered}>
+        <Heading
+          size="xxs"
+          color={color}
+          weight={weight}
+          isCentered={isCentered}
+          isInverted={isInverted}
+          isMarginless
+        >
           {children || href}
         </Heading>
       </a>
--- components/organisms/Footer.js
+++ components/organisms/Footer.js
@@ -2,74 +2,101 @@ import PropTypes from "prop-types";
 import Heading from "../atoms/Heading";
 import Paragraph from "../atoms/Paragraph";
 import Icon from "../atoms/Icon";
+import Link from "../atoms/Link";
+import PageContainer from "../layout/PageContainer";
 
-const Footer = ({ children }) => {
+const Footer = ({}) => {
   return (
     <>
-      <div className="footer">
-        <Heading>
-          <strong>Say Hello!</strong>
-        </Heading>
-        <Paragraph isInverted>it would be a pleasure to meet you</Paragraph>
-        <div className="icon-list">
-          <div className="item">
-            <Icon name="email" />
-            <Heading size="xxs" isInverted isMarginless>
-              glrodasz@gmail.com
-            </Heading>
+      <footer>
+        <PageContainer>
+          <div className="container">
+            <img className="logo" src={`/vectors/footer.svg`}></img>
+
+            <div className="footer">
+              <Heading>
+                <strong>Say Hello!</strong>
+              </Heading>
+              <Paragraph isInverted>
+                It would be a pleasure to meet you!
+              </Paragraph>
+              <div className="icon-list">
+                <div className="item">
+                  <Icon name="email" />
+                  <Link href="mailto:me@guillermorodas.com" isInverted>
+                    me(at)guillermorodas.com
+                  </Link>
+                </div>
+                <div className="item">
+                  <Icon name="link" />
+                  <Link href="https://guillermorodas.com" isInverted>
+                    guillermorodas.com
+                  </Link>
+                </div>
+                <div className="item">
+                  <Icon name="bubble" />
+                  <Link href="https://undefined.sh" isInverted>
+                    undefined.sh
+                  </Link>
+                </div>
+                <div className="item">
+                  <Icon name="cellphone" />
+                  <Link href="tel:+4607640378xx" isInverted>
+                    +(46) 076 403 78 xx
+                  </Link>
+                </div>
+              </div>
+              <div className="social-networks">
+                <Heading size="xxs" isInverted isMarginless>
+                  @glrodasz
+                </Heading>
+                <div>
+                  <Icon name="twitch" hasBackground />
+                  <Icon name="instagram" hasBackground />
+                  <Icon name="twitter" hasBackground />
+                  <Icon name="youtube" hasBackground />
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="item">
-            <Icon name="link" />
-            <Heading size="xxs" isInverted isMarginless>
-              guillermorodas.com
-            </Heading>
-          </div>
-          <div className="item">
-            <Icon name="bubble" />
-            <Heading size="xxs" isInverted isMarginless>
-              undefined.sh
-            </Heading>
-          </div>
-          <div className="item">
-            <Icon name="cellphone" />
-            <Heading size="xxs" isInverted isMarginless>
-              +(46) 076 403 7871
-            </Heading>
-          </div>
-        </div>
-        <div className="social-networks">
-          <Heading size="xxs" isInverted isMarginless>
-            @glrodasz
-          </Heading>
-          <div>
-            <Icon name="twitch" hasBackground />
-            <Icon name="instagram" hasBackground />
-            <Icon name="twitter" hasBackground />
-            <Icon name="youtube" hasBackground />
-          </div>
-        </div>
-      </div>
+        </PageContainer>
+      </footer>
+
       <style jsx>{`
+        footer {
+          padding: 80px 20px 0;
+          background: var(--black);
+        }
+
+        .container {
+          display: flex;
+          flex-direction: column-reverse;
+          justify-content: center;
+        }
+
+        .logo {
+          height: 200px;
+          width: 100%;
+          object-fit: none;
+          object-position: center 50px;
+        }
+
         .footer {
-          padding-top: 80px;
           display: flex;
           flex-direction: column;
           align-items: center;
-          background: var(--black) url("/vectors/footer.svg") no-repeat;
-          background-position: center calc(100% + 100px);
         }
 
         .icon-list {
           display: flex;
           flex-direction: column;
-          margin-top: 50px;
-          margin-bottom: 30px;
+          margin: 10px 0 30px;
+          gap: 15px;
         }
 
         .item {
           display: flex;
           align-items: center;
-          margin: 10px 0;
         }
 
         .item > :global(div) {
@@ -79,8 +106,31 @@ const Footer = ({ children }) => {
         .social-networks {
           display: flex;
           justify-content: space-around;
-          margin-bottom: 250px;
           align-items: center;
+        }
+
+        @media (min-width: 1140px) {
+          .container {
+            display: grid;
+            grid-template-columns: repeat(2, 1fr);
+            grid-template-rows: 1fr;
+            gap: 100px;
+          }
+
+          .footer {
+            align-items: flex-start;
+            grid-column: 2/3;
+          }
+
+          .social-networks {
+            margin-bottom: 0;
+          }
+
+          .logo {
+            height: 100%;
+            object-fit: contain;
+            object-position: left 150px;
+          }
         }
       `}</style>
     </>
