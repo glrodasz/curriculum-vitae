5dd3effe73a0a07b322d5b97f7dc64563a4b4f23
Guillermo Rodas <glrodasz@gmail.com>
2025-10-22 01:01:39 +0200

    chore: commit changes

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,6 @@ const index = () => {
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <div className="container">
         <Hero />
